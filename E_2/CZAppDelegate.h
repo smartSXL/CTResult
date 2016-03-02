342e9cd3d1625e3090fdec75e1d75f07b5d85d75
玲玲 <suxl@shicaidai.com>
2016-03-02 10:36:36 +0800

    添加CZAppDelegate.h文件

---
+++ E_2/CZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CZAppDelegate.h
+//  E_2
+//
+//  Created by Alvechen on 14-9-21.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
